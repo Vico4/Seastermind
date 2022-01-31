6f7f14fe3c1581e122cc079ef0a4683a9527f00a
Vico4 <victoire.stahl@gmail.com>
2022-01-31 15:43:33 +0100

    retrait des consol logs

--- src/Componants/line.js
+++ src/Componants/line.js
@@ -13,7 +13,6 @@ function Line({ id, solution }) {
   function ballClick(id) {
     let line_tmp = [...line];
     line_tmp[id] = line_tmp[id] === 3 ? 0 : line_tmp[id] + 1;
-    console.log(line_tmp);
     setLine(line_tmp);
   }
 
@@ -21,9 +20,6 @@ function Line({ id, solution }) {
     send(true);
     var sol = [...solution];
     let answer = [...line];
-    console.log(answer);
-    console.log(sol);
-    // commencer par comparer answer et sol
     let colorOk = 0;
     let allOk = 0;
     if (answer === sol) {
@@ -36,14 +32,11 @@ function Line({ id, solution }) {
           answer[i] = "o"
         }
       }
-      console.log(answer);
-      console.log(sol);
       for (let i = 0; i < 4; i++) {
         if (answer.includes(sol[i])) {
           colorOk += 1;
         }
       }
-      console.log(allOk, colorOk);
       sendResult([allOk, colorOk]);
     }
   }
